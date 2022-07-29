26e72c125b5dc15a01b59f33a15ae2a31bcddd32
samsouta <samxsouta@gmail.com>
2022-07-29 09:35:33 +0630

    first

---
+++ function.js
@@ -0,0 +1,16 @@
+// function expension 
+let one = function () {
+    let name = "samsouta";
+    return name;
+}
+
+// function statment
+
+function two () {
+    return "hello"
+};
+
+// IIFE
+(function() {
+    console.log("hello world")
+})()
